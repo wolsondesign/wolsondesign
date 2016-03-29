a5313b89eaa2d996a40322422558d950e514c5f7
Andrew Wolson <wolsondesign@gmail.com>
2016-03-29 12:49:04 -0400

    Setup SRC folder structure for SASS and JADE. :tiger:

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 This is the code for my portfolio(http://wolsondesign.com/).
 
 Local host setup for MAC. (Source: http://superuser.com/questions/878025/the-easiest-way-to-serve-a-page-to-localhost-on-os-x)
+
 1. Open terminal.
+
 2. Navigate to the folder (directory) containing the file you want to serve.
+
 3. php -S localhost:5000
+
 4. Go to http://localhost:5000 in the browser.
+
 5. Control+c to quit
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {  
-  return gulp.src('./src/sass/main.scss')
+  return gulp.src('./src/brand/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/main.scss', ['sass']);
+  gulp.watch('./src/brand/main.scss', ['sass']);
 });
 
 gulp.task('default', ['sass','sass:watch']);
