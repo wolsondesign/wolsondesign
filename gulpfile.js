38172090a6f348c340b4ee3543327ef4dafe7645
Andrew Wolson <wolsondesign@gmail.com>
2016-03-30 00:43:16 -0400

    Added CSS minification, Jade support, and remote server to gulp. Coded the index page in jade. :tiger:

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,46 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var rename = require('gulp-rename');
+var jade = require('gulp-jade');
+var affected = require('gulp-jade-find-affected');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 
-gulp.task('sass', function () {  
-  return gulp.src('./src/brand/main.scss')
+gulp.task('jade', function() {  
+  gulp.src('./src/canvas/*.jade')
+    .pipe(affected())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function () {
+  gulp.src('./src/brand/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(cssnano())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/brand/main.scss', ['sass']);
+gulp.task('browser-sync', function() {  
+    browserSync.init({
+      ghostMode: false,
+      notify: false,
+      reloadDelay: 500,
+       server: {
+          baseDir: './dist'
+       }
+    });
 });
 
-gulp.task('default', ['sass','sass:watch']);
+gulp.task('watch', function () {
+  gulp.watch('./src/brand/*.scss', ['sass']);
+  gulp.watch('./src/canvas/*.jade', ['jade']);
+});
+
+gulp.task('default', ['jade','sass','watch','browser-sync']);
